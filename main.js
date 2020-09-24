c80b0d2a93992734cd2358dd633a5026bc3cd60d
Denis Lima <denis@superclient.com.br>
2020-09-24 15:21:12 -0300

    add classe shape e ball

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// setup canvas
+// configurar canvas
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -6,95 +6,102 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
+// função para gerar números aleatórios
 
 function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+const num = Math.floor(Math.random() * (max - min + 1)) + min;
+return num;
 }
 
-class Ball {
-  Shape(x, y, velX, velY) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-  Ball()
-  draw() {
+class Shape {
+    constructor(x, y, velX, velY,  existe=true) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.existe = existe;
+        }
+}
+
+class Ball extends Shape {
+    constructor(x, y, velX, velY, existe=true, color, size) {
+        super(x, y, velX, velY, existe=true, color, size) 
+        this.color = color;
+        this.size = size;
+    }
+}
+
+Ball.prototype.draw = function() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-  }
-  update() {
+}
+
+Ball.prototype.update = function() {
     if ((this.x + this.size) >= width) {
-      this.velX = -(this.velX);
+        this.velX = -(this.velX);
     }
 
     if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
+        this.velX = -(this.velX);
     }
 
     if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
+        this.velY = -(this.velY);
     }
 
     if ((this.y - this.size) <= 0) {
-      this.velY = -(this.velY);
+        this.velY = -(this.velY);
     }
 
     this.x += this.velX;
     this.y += this.velY;
-  }
-  collisionDetect() {
+}
+
+Ball.prototype.collisionDetect = function() {
     for (let j = 0; j < balls.length; j++) {
-      if (!(this === balls[j])) {
+        if (!(this === balls[j])) {
         const dx = this.x - balls[j].x;
         const dy = this.y - balls[j].y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < this.size + balls[j].size) {
-          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
         }
-      }
     }
-  }
+    }
 }
-
-
-
 
 let balls = [];
 
 function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-  ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
 
-  while (balls.length < 25) {
+while (balls.length < 25) {
     let size = random(10,20);
     let ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
-      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-      size
+        //  bola sempre será desenhada com pelo menos a largura de uma bola
+        // de distancia longe das bordas da tela para evitar erros
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    true,
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
     );
-    balls.push(ball);
-  }
-
-  for (let i = 0; i < balls.length; i++) {
+balls.push(ball);
+}
+                                                
+for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
     balls[i].collisionDetect();
-  }
+}
 
-  requestAnimationFrame(loop);
+requestAnimationFrame(loop);
 }
 
 loop()
